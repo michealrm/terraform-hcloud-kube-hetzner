47bcec9147a44666e5ac64fe1845fd05d8bddd6d
Micheal <8922961+michealrm@users.noreply.github.com>
2025-05-01 00:40:14 -0500

    Istio values

--- variables.tf
+++ variables.tf
@@ -560,6 +560,30 @@ variable "haproxy_values" {
   description = "Helm values file to pass to haproxy as 'valuesContent' at the HelmChart, overriding the default."
 }
 
+variable "istio_version" {
+  description = "Version of Istio helm chart. See https://github.com/istio/istio/releases for available versions."
+  type        = string
+  default     = ""
+}
+
+variable "istio_autoscaling" {
+  description = "Enable Horizontal Pod Autoscaler for Istio."
+  type        = bool
+  default     = true
+}
+
+variable "istio_ambient_enabled" {
+  description = "Enable Istio Ambient mode, which deploys ztunnel as a DaemonSet."
+  type        = bool
+  default     = false
+}
+
+variable "istio_values" {
+  description = "Additional helm values file to pass to Istio as 'valuesContent' at the HelmChart."
+  type        = string
+  default     = ""
+}
+
 variable "allow_scheduling_on_control_plane" {
   type        = bool
   default     = false
